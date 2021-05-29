5a9d6f6fc8fa3fa78e10d592c678ff05d43d73d7
Julian Easterling <julian@julianscorner.com>
2021-05-29 15:19:29 -0400

    Added a docker container for Ansible so I can do Ansible and not worry what platform the controller is on.

---
+++ ansible/build.cmd
@@ -0,0 +1,7 @@
+@echo off
+setlocal
+set VERSION=4.0.0
+
+docker build --pull --no-cache --progress plain -t dcjulian29/ansible:%VERSION% .
+docker tag dcjulian29/ansible:%VERSION% dcjulian29/ansible:latest
+docker push dcjulian29/ansible --all-tags
---
+++ ansible/run.cmd
@@ -0,0 +1,16 @@
+@docker run -it --rm -v "C:\code\ansible:/etc/ansible:ro" dcjulian29/ansible %*
+
+exit 0
+
+docker run -it --rm --volume "$(pwd)":/ansible --workdir /ansible `
+--env "AWS_ACCESS_KEY_ID='<AWS_Access_Key_ID>'" `
+--env "AWS_SECRET_ACCESS_KEY='<AWS_Secret_Access_Key>'" `
+dcjulian29/ansible
+
+##AZURE
+docker run -it --rm --volume "$(pwd)":/ansible --workdir /ansible `
+--env "AZURE_SUBSCRIPTION_ID=<Azure_Subscription_ID>" `
+--env "AZURE_CLIENT_ID=<Service_Principal_Application_ID>" `
+--env "AZURE_SECRET=<Service_Principal_Password>" `
+--env "AZURE_TENANT=<Azure_Tenant>" `
+dcjulian29/ansible
