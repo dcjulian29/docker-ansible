da5ade15d412c7f7aec328cc5cb1072f25cae6a8
Julian Easterling <julian@julianscorner.com>
2021-06-22 12:11:28 -0400

    Updated my Ansible container to 4.1.0

--- ansible/build.cmd
+++ ansible/build.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-set VERSION=4.0.0
+set VERSION=4.1.0
 
 docker build --pull --no-cache --progress plain -t dcjulian29/ansible:%VERSION% .
 docker tag dcjulian29/ansible:%VERSION% dcjulian29/ansible:latest
