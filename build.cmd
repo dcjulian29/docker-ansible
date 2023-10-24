08f75b7a8f50e53a1194c95cd875754e54d26fcc
Julian Easterling <julian@julianscorner.com>
2023-10-23 21:36:19 -0400

    Fixed build so that gorelease doesn't run if docker fails

--- build.cmd
+++ build.cmd
@@ -5,15 +5,19 @@ pushd %~dp0
 
 for /f "delims=" %%x in (version) do set ANSIBLE_VERSION=%%x
 
-docker build --build-arg ANSIBLE_VERSION=%ANSIBLE_VERSION% --progress plain ^
-  -t dcjulian29/ansible:%ANSIBLE_VERSION% .
+docker build --no-cache --build-arg ANSIBLE_VERSION=%ANSIBLE_VERSION% ^
+  --pull --progress plain -t dcjulian29/ansible:%ANSIBLE_VERSION% .
 
-if %errorlevel% neq 0 popd;exit /b %errorlevel%
-
-popd
+if %errorlevel% neq 0 goto FIN
 
 docker tag dcjulian29/ansible:%ANSIBLE_VERSION% dcjulian29/ansible:latest
 
-goreleaser release --snapshot --skip-publish --clean
+echo.
+
+goreleaser release --skip-validate --skip-publish --clean
+
+:FIN
+
+popd
 
 endlocal
