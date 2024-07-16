01e6e3ac054f531f27200fe1cff9779fa9c59e3f
Julian Easterling <julian@julianscorner.com>
2024-07-16 18:43:20 -0400

    Don't tag build outputs as latest

--- build.cmd
+++ build.cmd
@@ -5,16 +5,13 @@ pushd %~dp0
 
 for /f "delims=" %%x in (version) do set ANSIBLE_VERSION=%%x
 
-docker build --no-cache --build-arg ANSIBLE_VERSION=%ANSIBLE_VERSION% ^
-  --pull --progress plain -t dcjulian29/ansible:%ANSIBLE_VERSION% .
+docker build --build-arg ANSIBLE_VERSION=%ANSIBLE_VERSION% -t dcjulian29/ansible:%ANSIBLE_VERSION% .
 
 if %errorlevel% neq 0 goto FIN
 
-docker tag dcjulian29/ansible:%ANSIBLE_VERSION% dcjulian29/ansible:latest
-
 echo.
 
-goreleaser release --skip-validate --skip-publish --clean
+goreleaser release --skip publish,validate --clean
 
 :FIN
 
