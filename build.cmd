2d0dab5b1a8cb16b2375d12e1cfcc26b07d88d12
Julian Easterling <julian@julianscorner.com>
2025-12-16 13:14:41 -0500

    Changed progress during build to plain so I can see what is happening.

--- build.cmd
+++ build.cmd
@@ -5,7 +5,7 @@ pushd %~dp0
 
 for /f "delims=" %%x in (version) do set ANSIBLE_VERSION=%%x
 
-docker build --build-arg ANSIBLE_VERSION=%ANSIBLE_VERSION% -t dcjulian29/ansible:%ANSIBLE_VERSION% .
+docker build --progress plain --pull --build-arg ANSIBLE_VERSION=%ANSIBLE_VERSION% -t dcjulian29/ansible:%ANSIBLE_VERSION% .
 
 if %errorlevel% neq 0 goto FIN
 
